09aae42f5fc093e788df672aa12f8dd6347fa501
Hải Sơn ❤️ <sonnguyennr111@gmail.com>
2025-03-04 23:29:17 +0700

    Them thu muc Day07

--- Day07/nhs-leeson07/src/index.js
+++ Day07/nhs-leeson07/src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import NhsApp from './NhsApp';
 
+
 const NhsRoot = ReactDOM.createRoot(document.getElementById('NhsRoot'));
 NhsRoot.render(
   <React.StrictMode>
-    <NhsApp />
+   <NhsApp />
   </React.StrictMode>
 );
 
