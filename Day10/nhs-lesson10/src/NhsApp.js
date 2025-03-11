9c0acf70f2e6ac48eb7706c2c65d40c74325f647
Hải Sơn ❤️ <sonnguyennr111@gmail.com>
2025-03-11 23:50:24 +0700

    Them thu muc Day10

---
+++ Day10/nhs-lesson10/src/NhsApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NhsApp() {
+  return (
+    <div className='container border my-2'> 
+          <h1>Nguyễn Hải Sơn</h1>
+      
+    </div>
+  )
+}
